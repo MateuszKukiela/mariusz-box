b9359aa5e5dac2194945a04f5a3cc45bab017ceb
Mateusz Kukiela <mateuszkukiela@gmail.com>
2023-08-17 17:52:08 +0200

    Update kiepski downloader

--- kiepski_downloader/kiepski_downloader.py
+++ kiepski_downloader/kiepski_downloader.py
@@ -21,44 +21,48 @@ def read_env_file(filename: str) -> Dict[str, str]:
     return ENV
 
 
-def read_episodes(filename: str) -> Dict[int, List[Tuple[str, str]]]:
+def read_episodes(filename: str) -> List[Tuple[str, str]]:
     """
-    Read the episodes from a text file and return them as a dictionary,
-    with season number as the key and a list of episode tuples (episode_title, episode_url) as the value.
+    Read the episodes from a text file and return them as a list of tuples.
+    Each tuple contains the episode name (with the number) and the URL.
     """
     with open(filename, "r") as file:
         lines = file.readlines()
 
-    episodes = {}
-    season = None
-    episode_title = None
-
-    for line in lines:
-        line = line.strip()
-        if "SEZON" in line.upper():
-            season = int(line.split()[-1])
-            episodes[season] = []
-        elif line:
-            if episode_title is None:
-                # This line is expected to be the episode title
-                episode_title = line.split(". ", 1)[-1].strip()
-            else:
-                # This line is expected to be the episode URL
-                episode_url = line
-                episodes[season].append((episode_title, episode_url))
-                episode_title = None  # Reset for next episode
+    episodes = []
+    for i in range(len(lines)):
+        if lines[i].strip() and "SEZON" not in lines[i].upper():
+            episode_name = lines[i].strip()
+            if i + 1 < len(lines):
+                episode_url = lines[i + 1].strip()
+                episodes.append((episode_name, episode_url))
 
     return episodes
 
 
-def download_episode(season: int, episode_number: int, episode_info: Tuple[str, str], path: str) -> None:
+def get_season_and_episode(
+        episode_number: int, seasons: Dict[int, Tuple[int, int]]
+) -> Tuple[Optional[int], Optional[int]]:
+    """
+    Find the season and the adjusted episode number based on the original episode number.
+    """
+    for season, (start, end) in seasons.items():
+        if start <= episode_number <= end:
+            return season, episode_number - start + 1
+    return None, None
+
+
+def download_episode(
+        download_info: Tuple[str, str], path: str, seasons: Dict[int, Tuple[int, int]]
+) -> None:
     """
     Download the episode if it doesn't exist in the path.
     """
-    episode_title, episode_url = episode_info
-
     try:
-        file_path = f"{os.path.join(path, f'tv/Swiat wedlug Kiepskich/Season {season:02}', f'S{season:02}E{episode_number:03}')}.mp4"
+        episode_number = int(download_info[0].split(" ")[0])
+        season, episode_number_adjusted = get_season_and_episode(episode_number, seasons)
+
+        file_path = f"{os.path.join(path, f'tv/Swiat wedlug Kiepskich/Season {season:02}', f'S{season:02}E{episode_number_adjusted:03}')}.mp4"
 
         if not os.path.exists(os.path.dirname(file_path)):
             try:
@@ -68,27 +72,67 @@ def download_episode(season: int, episode_number: int, episode_info: Tuple[str, 
                     raise
 
         if not os.path.isfile(file_path):
-            print(f"Downloading {episode_title}")
-            print(f"URL: {episode_url}")
-            print(f"Path: {file_path}")
-            urllib.request.urlretrieve(episode_url, file_path)
+            print(f"Downloading {download_info[0]}")
+            urllib.request.urlretrieve(download_info[1], file_path)
 
     except urllib.error.HTTPError as e:
-        print(f"HTTP Error for {episode_title}: {e.code} - {e.reason}")
+        print(f"HTTP Error for {download_info[0]}: {e.code} - {e.reason}")
+
     except urllib.error.URLError as e:
-        print(f"URL Error for {episode_title}: {e.reason}")
+        print(f"URL Error for {download_info[0]}: {e.reason}")
+
+    except ValueError as e:
+        print(f"Value Error for {download_info[0]}: {e}")
+
     except Exception as e:
-        print(f"Failed to download {episode_title}: {e}")
+        print(f"Failed to download {download_info[0]}: {e}")
 
 
 def main() -> None:
     env = read_env_file("../.env")
     path = env["ROOT_MEDIA"]
-    episodes = read_episodes("episodes.txt")
 
-    for season, episode_list in episodes.items():
-        for episode_number, episode_info in enumerate(episode_list, 1):
-            download_episode(season, episode_number, episode_info, path)
+    seasons = {
+        1: (1, 38),
+        2: (39, 76),
+        3: (77, 110),
+        4: (111, 133),
+        5: (134, 154),
+        6: (155, 170),
+        7: (171, 186),
+        8: (187, 202),
+        9: (203, 244),
+        10: (245, 265),
+        11: (266, 278),
+        12: (279, 292),
+        13: (293, 307),
+        14: (308, 322),
+        15: (323, 337),
+        16: (338, 352),
+        17: (353, 365),
+        18: (366, 379),
+        19: (380, 392),
+        20: (393, 405),
+        21: (406, 418),
+        22: (419, 431),
+        23: (432, 444),
+        24: (445, 456),
+        25: (457, 468),
+        26: (469, 480),
+        27: (481, 492),
+        28: (493, 504),
+        29: (505, 516),
+        30: (517, 528),
+        31: (529, 540),
+        32: (541, 552),
+        33: (553, 564),
+        34: (565, 576),
+        35: (577, 588),
+    }
+
+    episodes = read_episodes("episodes.txt")
+    for episode_info in episodes:
+        download_episode(episode_info, path, seasons)
 
 
 if __name__ == "__main__":
